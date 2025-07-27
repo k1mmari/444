764f3d990eb668b882186b138c64c54901eebd0a
Alfredo-Catzin <alfredocatzin10@gmail.com>
2025-07-26 21:53:44 -0700

    Reconfigured backend to be a maven project with corresponding dependencies in the pom file, along with spring boot framework to interact with html files and database. -Application uses jdk 21 for compatibility with spring boot version -JAVA_HOME and path may need to be updated if not currently at jdk21 -mvn spring-boot:run in the command shell terminal, be sure you cd into backend folder where pom file resides

---
+++ backend/src/test/java/com/sweetshopdb/sweetShopApp.java
@@ -0,0 +1,13 @@
+package com.sweetshopdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class sweetShopApp {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
